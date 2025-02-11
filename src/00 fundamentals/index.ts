6f5b788b66f4a20034ae7c7352f13909f80459e0
Sasha Shevchenko <acidi@icloud.com>
2025-02-10 20:26:15 -0800

    replaces js triggered animation with pseudo-class triggered

--- src/00 fundamentals/index.ts
+++ src/00 fundamentals/index.ts
@@ -1,14 +1,1 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementsByClassName(
-    "ui-button"
-  )[0] as HTMLButtonElement;
-  const input = document.getElementsByClassName(
-    "ui-input"
-  )[0] as HTMLInputElement;
-  button.onclick = () => {
-    input.classList.add("ui-input-invalid");
-    input.addEventListener("animationend", () => {
-      input.classList.remove("ui-input-invalid");
-    });
-  };
-});
+window.addEventListener("DOMContentLoaded", () => {});
