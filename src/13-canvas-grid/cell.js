1aebe7f790b0a6daf5bb678d0ea238ab8b39e5e6
Sasha Shevchenko <acidi@icloud.com>
2025-04-24 23:28:13 -0700

    minor optimization

--- src/13-canvas-grid/cell.js
+++ src/13-canvas-grid/cell.js
@@ -15,7 +15,7 @@ export class Cell {
     this.vx = 0;
     this.vy = 0;
 
-    // -- Moving cells --
+    // -- Moving cell --
     // these two define cell position
     this.positionX = this.effect.width / 2;
     this.positionY = this.effect.height / 2;
@@ -27,9 +27,11 @@ export class Cell {
     this.randomize = Math.random() * 20 + 2;
     this.ease = 0.01;
     this.friction = 0.8;
+
+    this.start();
   }
   draw(context) {
-    // context.strokeRect(this.x, this.y, this.width, this.height);
+    context.strokeRect(this.positionX, this.positionY, this.width, this.height);
     context.drawImage(
       this.image,
 
@@ -46,12 +48,23 @@ export class Cell {
       this.height
     );
   }
-  update() {
-    // cell position
+  // because of the condition below we need to initialize
+  // speedX and speedY in the constructor
+  start() {
     this.speedX = (this.x - this.positionX) / this.randomize;
     this.speedY = (this.y - this.positionY) / this.randomize;
-    this.positionX += this.speedX;
-    this.positionY += this.speedY;
+  }
+  update() {
+    // cell position
+    // condition stops movement calculations when the cell is in place
+    if (Math.abs(this.speedX) > 0.1 || Math.abs(this.speedY) > 0.1) {
+      // FIXME: the console never stops
+      console.log("speed update");
+      this.speedX = (this.x - this.positionX) / this.randomize;
+      this.speedY = (this.y - this.positionY) / this.randomize;
+      this.positionX += this.speedX;
+      this.positionY += this.speedY;
+    }
 
     // crop
     const dx = this.effect.mouse.x - this.x;
