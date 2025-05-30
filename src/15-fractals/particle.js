66c6c7bd2a1c2e78b533b109e2977e3a89a59229
Sasha Shevchenko <acidi@icloud.com>
2025-05-29 18:52:20 -0700

    adds rotation

--- src/15-fractals/particle.js
+++ src/15-fractals/particle.js
@@ -9,22 +9,38 @@ export class Particle {
     this.width = this.image.width * this.sizeModifier;
     this.height = this.image.height * this.sizeModifier;
     this.speed = Math.random() + 0.2;
+    this.angle = Math.random() * Math.PI * 2;
+    this.va = Math.random() * 0.02 - 0.01;
   }
 
   update() {
+    this.angle += this.va;
+
     this.x += this.speed;
     this.y += this.speed;
 
     if (this.x > this.canvasWidth + this.width) {
       this.x = -this.width;
     }
-
     if (this.y > this.canvasHeight + this.height) {
       this.y = -this.height;
     }
   }
 
   draw(context) {
-    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    context.save();
+
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+
+    context.drawImage(
+      this.image,
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    );
+
+    context.restore();
   }
 }
